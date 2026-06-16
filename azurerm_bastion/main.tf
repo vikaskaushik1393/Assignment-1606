23ce76b0bece0b03ee32a32559bd6284881ff861
vikas <vikaskaushik1393@gmail.com>
2026-06-16 19:57:05 +0530

    add public

---
+++ azurerm_bastion/main.tf
@@ -0,0 +1,12 @@
+resource "azurerm_bastion_host" "example" {
+    for_each = var.azure_bastion
+  name                = each.value.name
+  location            = each.value.location
+  resource_group_name = each.value.resource_group_name
+
+  ip_configuration {
+    name                 = each.value.ip_configuration.name
+    subnet_id            = each.value.ip_configuration.subnet_id
+    public_ip_address_id = each.value.ip_configuration.public_ip_address_id
+  }
+}
